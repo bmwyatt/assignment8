b4dcd6075fdf2200088965fd9bafc1a3e2426cc5
bmwyatt <briana.wyatt@okstate.edu>
2016-10-05 11:02:09 -0500

    Version 1- no data

---
+++ houseelf-analysis.R
@@ -0,0 +1,7 @@
+# PLNT 5110 Homework 8
+# BMW 5 October 2016
+
+# Set working directory (for Briana only)
+setwd("C:/Users/Briana/Dropbox (Personal)/Courses/PLNT 5110/Week 8")
+
+# This script analyzes house elf data from Dr. Granger.
