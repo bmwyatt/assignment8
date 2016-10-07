be73dcd6902ad99a6952d2ed4b7811989e51c8bb
bmwyatt <briana.wyatt@okstate.edu>
2016-10-07 11:00:45 -0500

    Added GC function

--- houseelf-analysis.R
+++ houseelf-analysis.R
@@ -3,7 +3,16 @@
 
 # Set working directory (for Briana only)
 setwd("C:/Users/Briana/Dropbox (Personal)/Courses/PLNT 5110/Week 8")
+library(stringr)
 
 # This script analyzes house elf data from Dr. Granger.
 
 file <- read.csv('data/houseelf_earlength_dna_data_1.csv')
+
+# Problem 6
+
+#Define function
+gc <- function(sequence){
+  GC <- str_count(sequence, "[gGcC]")
+  gc_content <- (GC) / str_length(sequence) * 100
+  return(gc_content)}
