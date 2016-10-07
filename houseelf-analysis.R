2e2d269bbf4eaf8eb41386dface5a3f950a15a25
bmwyatt <briana.wyatt@okstate.edu>
2016-10-07 11:15:37 -0500

    Completed script and .csv output

--- houseelf-analysis.R
+++ houseelf-analysis.R
@@ -3,10 +3,12 @@
 
 # Set working directory (for Briana only)
 setwd("C:/Users/Briana/Dropbox (Personal)/Courses/PLNT 5110/Week 8")
+
 library(stringr)
 
 # This script analyzes house elf data from Dr. Granger.
 
+# Import data
 file <- read.csv('data/houseelf_earlength_dna_data_1.csv')
 
 # Problem 6
@@ -16,3 +18,18 @@ gc <- function(sequence){
   GC <- str_count(sequence, "[gGcC]")
   gc_content <- (GC) / str_length(sequence) * 100
   return(gc_content)}
+
+# Read Dr. Alderman's function into environment
+source('classify_ear_size.R')
+
+# Get size classes
+class <- get_size_class(file$earlength)
+
+# Get GC contents
+gc_content <- gc(file$dnaseq)
+
+# Create data table
+table <-data.frame(ID = file$id, Ear_Length_Class = class, GC_content = gc_content)
+
+# Export data
+write.csv(table,'assignment8.csv')
